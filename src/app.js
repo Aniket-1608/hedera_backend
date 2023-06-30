51cc0c86e987788e03a0145ca5ac5d202d6c03cb
Aniket <aniketjanbandhu2002@gmail.com>
2023-06-30 11:10:45 +0530

    Third Commit

--- src/app.js
+++ src/app.js
@@ -14,96 +14,40 @@ if(process.env.NODE_ENV !== 'production'){
 const PORT = process.env.PORT || 3000;
 const CONNECTION = process.env.CONNECTION;
 
-// const customers =[
-//     {
-//         name: "Aniket",
-//         industry: "Pop music"
-//     },{
-//         name: "Khali",
-//         industry: "wrestling"
-//     },{
-//         name:"Akshay Kumar",
-//         industry: "Artist"
-//     }
-// ];
 
-const customer =new Customer.customer({
-    name: "Devil",
-    industry: "Hell"
-});
-// customer.save();
-
-const user = new Customer.user({
-    walletAddress: "0xfaahbksnfkansoiewinajsnfjasda",
-    numOfSeeds: 5,
-    manureBags: 2,
-    treesGrown: 5,
-    level: 10,
-    nftsEarned: 8
-
-})
-// user.save()
-    
-
-//  for the seed document
 const seed = new Customer.seed({
-    seedId: 2,
-    stage: 2,
-    seedOwner: "Diablo",
-    coordinates: "441.0.109",
-    seedType: "Sappling",
-    age: 8,
-    hrsToDie: 20,
-    seedHealth: "Healthy",
-    isTree: true,
-    isDead: false
+    seedId: 1,
+    stage: 'seed',
+    seedOwner: 'John Doe',
+    timeofPlanting: new Date(),
+    coordinates: '1,2',
+    seedType: 'common',
+    age: 2,
+    hrsToDie: 15,
+    seedHealth: "red",
+    isTree: false,
+    isDead: false,
+    isWatered: false 
 });
+// const result= seed.updateAge();
+// console.log({result});
+// user.save()
 
+
+// ###API CALLS####
 app.get('/',(req,res) => {
-    res.json(seed);
+    res.send(seed);
 });
 
-app.get('/api/customers', async (req,res) => {
+app.get('/seed', async(req,res) => {
     try {
-        const result = await Customer.customer.find();
-        res.json({"Customers": result});
+    //     const result = await seed.updateAge();
+    // res.json({result});
+    console.log(seed);
     } catch (e) {
         res.status(500).json({error: e.message});
-    }    
-});
-
-app.get('/api/customers/:id', async(req, res) => {
-    console.log({
-        requestParams: req.params,
-        requestQuery: req.query
-    });
-    try {
-        const customerId = req.params.id;
-        console.log(customerId);
-        const customer = await Customer.customer.findById(customerId);
-        console.log(customer);
+    }
     
-        if(!customer){
-            res.status(404).json({error: 'user not found'});
-        }
-        else{
-            res.json({customer});
-        }
-        
-    } catch (e) {
-        res.status(500).json({error: 'something went wrong' })
-    }
-});
-
-app.put('/api/customers/:id', async (req, res) => {
-    try {
-        const customerId = req.params.id;
-    const result = await Customer.customer.findOneAndReplace({_id: customerId}, req.body, {new : true});
-    // console.log(result);
-    res.json({result});
-    } catch (e) {
-        req.status(500).json({error: 'Something went wrong'});
-    }
 });
 
 app.post('/', (req, res) => {
@@ -111,16 +55,6 @@ app.post('/', (req, res) => {
 });
 
 //for the customer document
-app.post('/api/customers', async (req, res) => {
-    console.log(req.body);
-    const customer = new Customer.customer(req.body);
-    try {
-        await customer.save();
-        res.status(201).json({customer}); 
-    } catch (e) {
-        res.status(400).json({error: e.message});
-    }
-});
 
 //for the seed document
 app.post('/api/seeds', async (req, res) => {
@@ -146,24 +80,24 @@ app.post('/api/users', async (req, res) => {
     }
 });
 
-
-app.put('/api/customers/seeds/:id', async (req, res) => {
+// CALL TO THE ADDMANURE FUNCTION.
+app.put('/api/seeds/:id', async (req, res) => {
     try {
         const seedId = req.params.id;
-        console.log(seedId);
-        const result = await Customer.seed.findOneAndReplace({_id: seedId}, req.body, {new : true});
-        console.log(result);
-        res.json({result});
+        const find = await Customer.seed.findOne({_id:seedId});
+        // console.log(find);
+        // const manureAdded = await find.
+        // const result = await Customer.seed.findOneAndUpdate({_id: seedId}, manureAdded, {new : true});
+        console.log(manureAdded);
+        res.json({find});
     } catch (e) {
-        req.status(500).json({error: 'Something went wrong'});
+        res.status(500).json({error: 'Something went wrong'});
     }
 });
-
 
 const start = async() => { 
     try {
         await mongoose.connect(CONNECTION);
-
         app.listen(PORT, () => {
         console.log('App listening on port:' + PORT)
     });
--- src/models/customer.js
+++ src/models/customer.js
@@ -1,3 +1,4 @@
+const { magicLink, useAirdropNFT } = require('@thirdweb-dev/react');
 const mongoose = require('mongoose');
  
 const customerSchema = new mongoose.Schema({
@@ -72,7 +73,7 @@ const seedsSchema = new mongoose.Schema({
     },
     seedType: {
         type: String,
-        default: "common",
+        default: "common"
         // required: true
     },
     age: {
@@ -82,7 +83,7 @@ const seedsSchema = new mongoose.Schema({
     },
     hrsToDie: {
         type: Number,
-        dafault:24
+        dafault:0
         // required: true
     },
     seedHealth: {
@@ -99,10 +100,99 @@ const seedsSchema = new mongoose.Schema({
         type: Boolean,
         default: false
         // required: true
+    },
+    isWatered: {
+        type: Boolean,
+        default: false
+        // required: true
     }
 
 });
+
 const seed = mongoose.model('seed', seedsSchema);
+
+seedsSchema.methods.updateAge = function() {
+    if(this.isDead != true) {
+    const now = new Date();
+    const timeDiff = Math.abs(now - this.timeofPlanting);
+    const hoursDiff = Math.ceil(timeDiff / (1000 * 60 * 60));
+    if (hoursDiff >= this.hrsToDie) {
+        this.age += Math.floor(hoursDiff / this.hrsToDie);
+        this.timeofPlanting = now;
+    }
+    } else {
+        console.log("The sappling is dead.");
+    }
+  };
+
+  seedsSchema.methods.addManure = function() {
+    return(this.age += 2);
+    //make an api call to the frontend
+  }
+
+  seedsSchema.methods.checkAge = function() {
+    if(this.isDead != true){if(this.age <= 4){
+        this.stage == "Seed";
+        //make an api call to the frontend
+    }
+    else if(this.age >4 || this.age < 14){
+        this.stage == "Plant";
+        //make an api call to the frontend
+    }
+    else {
+        this.stage == "Tree";
+        this.isTree == true;
+        //make an api call to the frontend
+    }}
+    else {
+        this.stage == "Dead";
+        //make an api call to the frontend
+    }
+  }
+
+seedsSchema.methods.seedfitness = function() {
+    if(this.isWatered != true){if(this.hrsToDie>12 && this.hrsToDie<=24){
+        this.seedHealth == "Green";
+        //make an api call to the frontend
+    }
+    else if(this.hrsToDie>6 && this.hrsToDie<=12){
+        this.seedHealth == "Yellow";
+        //make an api call to the frontend
+    }
+    else{
+        this.seedHealth == "Red";
+        //make an api call to the frontend
+    }}
+    else {
+        this.hrsToDie == 24;
+    }
+}
+seedsSchema.methods.manureBagsLeft = function() {
+    this.manureBags -=1;
+    //make an api call to the frontend
+}
+
+async function main() {
+    const seed1 = new seed({
+        seedId: 1,
+        stage: 'seed',
+        seedOwner: 'John Doe',
+        timeofPlanting: new Date(),
+        coordinates: '1,2',
+        seedType: 'common',
+        age: 2,
+        hrsToDie: 15,
+        seedHealth: "red",
+        isTree: false,
+        isDead: false,
+        isWatered: false 
+    });
+    // let user = seed1;
+    // console.log({user});
+    // const manureAdded = await user.addManure();
+    // console.log({manureAdded});
+}
+main();
 
 
 module.exports = {
